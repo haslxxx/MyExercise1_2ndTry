bf4613dc7d7a3ea1c5067645f5225e847b79c058
Harald Hurtig <haraldhurtig@noreply.users.github.com>
2018-03-22 18:10:03 +0100

    Status: avtivity_main.xml, fragment_main.xml rudimentär fertig. Fehler in MainActivity.java

--- app/src/main/java/com/example/ego/and_exercise1_ganzselbst/MainActivity.java
+++ app/src/main/java/com/example/ego/and_exercise1_ganzselbst/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.ego.and_exercise1_ganzselbst;
 
+import android.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -7,7 +8,15 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // TODO (1.1)hier kommen wir rein wenn der session manager die app aufruft ?? oder
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // da wir ja gleich weiterwollen zum Fragment, ohne irgendeine anzeige in der activity_main --> ein fragment machen
+        // TODO (1.2) wenn noch kein  fragment am stack ?? oder so ... eigentlich weiß ich da jetzt nicht warum ich's tu
+        if (savedInstanceState == null){
+            Fragment myMainFragment = new Main_Fragmentum(); //wir machen mal ein fragmentobjekt
+            getSupportFragmentManager().beginTransaction().add(R.id.leerer_container, myMainFragment).commit();
+        }
     }
 }
---
+++ app/src/main/java/com/example/ego/and_exercise1_ganzselbst/Main_Fragmentum.java
@@ -0,0 +1,10 @@
+package com.example.ego.and_exercise1_ganzselbst;
+
+import android.app.Fragment;
+
+/**
+ * Created by ego on 22.03.2018.
+ */
+
+public class Main_Fragmentum extends Fragment {
+}

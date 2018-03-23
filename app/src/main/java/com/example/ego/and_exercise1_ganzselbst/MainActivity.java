84efe531c3062bdf21671471dc67bab15cdd10b4
Harald Hurtig <haraldhurtig@noreply.users.github.com>
2018-03-23 11:00:19 +0100

    Nach Unüberbrückbarem fehler in MainActivity  abgebrochen

--- app/src/main/java/com/example/ego/and_exercise1_ganzselbst/MainActivity.java
+++ app/src/main/java/com/example/ego/and_exercise1_ganzselbst/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.ego.and_exercise1_ganzselbst;
 
-import android.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+
+import android.support.annotation.Nullable;
+// TODO (1.0) was tut das nullable
+
+// import com.example.ego.and_exercise1_ganzselbst.R; // der referenzlösung nachgebaut .. bringt aber auch nix
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         // TODO (1.1)hier kommen wir rein wenn der session manager die app aufruft ?? oder
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -15,8 +21,10 @@ public class MainActivity extends AppCompatActivity {
         // da wir ja gleich weiterwollen zum Fragment, ohne irgendeine anzeige in der activity_main --> ein fragment machen
         // TODO (1.2) wenn noch kein  fragment am stack ?? oder so ... eigentlich weiß ich da jetzt nicht warum ich's tu
         if (savedInstanceState == null){
-            Fragment myMainFragment = new Main_Fragmentum(); //wir machen mal ein fragmentobjekt
-            getSupportFragmentManager().beginTransaction().add(R.id.leerer_container, myMainFragment).commit();
+//            Fragment myMainFragment = new Main_Fragmentum(); //wir machen mal ein fragmentobjekt
+//            getSupportFragmentManager().beginTransaction().add(R.id.leerer_container, myMainFragment).commit();
+            getSupportFragmentManager().beginTransaction().add(R.id.empty_container, new Main_Fragmentum()).commit();
+            // TODO (1.3  finde den FEHLER .. aber wie ??
         }
     }
 }
